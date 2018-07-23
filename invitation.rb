ec9ef46d80dc6f5b4b3de2910b384fd3f44b17be
madddanz <maddiedanzberger@gmail.com>
2018-07-23 19:40:40 +0000

    automatically backed up by learn

--- invitation.rb
+++ invitation.rb
@@ -3,8 +3,8 @@ You are codrially invited to #{party_name} on #{date} at #{time}. Please RSVP no
 
 Sincerely, 
 Harry Potter"
-puts guest_name = "hermione
-puts party_name = "the Best Halloween Party Ever"
+puts guest_name = "hermione"
+puts party_name = "Best Halloween Party Ever"
 puts date = "October 31"
 puts time = "6 pm"
 puts host_name = "Harry Potter"
