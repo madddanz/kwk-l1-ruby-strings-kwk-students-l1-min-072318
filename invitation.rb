09dd1187d08d3e8e80858ed2b1b1454ff62eafcd
madddanz <maddiedanzberger@gmail.com>
2018-07-23 19:45:59 +0000

    automatically backed up by learn

--- invitation.rb
+++ invitation.rb
@@ -1,5 +1,5 @@
 puts "Dear #{guest_name}, 
-You are codrially invited to #{party_name} on #{date} at #{time}. Please RSVP no later than October 30.
+You are codrially invited to the #{party_name} on #{date} at #{time}. Please RSVP no later than October 30.
 
 Sincerely, 
 Harry Potter"
@@ -8,5 +8,5 @@ guest_name = gets.chomp.capitalize
 puts "What is the name of your party?"
 party_name = gets.chomp.capitalize
 puts "When is your party?"
-date = gets.chomp.capitalize
+date = gets.chomp
 puts "What is the time of your party?"
